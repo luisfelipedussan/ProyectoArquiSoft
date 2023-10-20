b04f6b63ff95a9a68611682b702ef67cf651e3bf
luisfelipedussan <lf.dussan@uniandes.edu.co>
2023-10-19 22:57:55 -0500

    arreglos

--- ProyectoArquiSoft/settings.py
+++ ProyectoArquiSoft/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ProyectoArquiSoft.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(BASE_DIR, 'ProyectoArquiSoft', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
