e179d19bc9cb4a5f3fc8bba270f15e528342a920
luisfelipedussan <lf.dussan@uniandes.edu.co>
2023-10-20 09:56:10 -0500

    cookies

--- ProyectoArquiSoft/settings.py
+++ ProyectoArquiSoft/settings.py
@@ -141,3 +141,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_CREDENTIALS = True
